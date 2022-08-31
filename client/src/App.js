9c393df10be784a8969e7f2d92a442d6f5e44685
Vikas Shenoy <p.vikas.shenoy@gmail.com>
2022-08-30 19:14:48 -0700

    Added Menu funtionality to all 3 pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Container } from "semantic-ui-react";
 function App() {
   return (
-    <div>
-      {" "}
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
